871916e4e32083944b701e32a7d41a4e8847bc95
zhangchanjuan <zhangchanjuan@agree.com.cn>
2023-02-20 15:19:05 +0800

    提交测试demo

---
+++ vue-template-for-test/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <DocxPreview />
+  <CreatedTest />
+</template>
+
+<script>
+import DocxPreview from './components/docx-preview/DocxPreview.vue'
+import CreatedTest from './components/created-test-for-async/CreatedTest.vue'
+
+export default {
+  name: 'App',
+  components: {
+    DocxPreview,
+    CreatedTest
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue-template-for-test/src/components/created-test-for-async/CreatedTest.vue
@@ -0,0 +1,44 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+import Test from "./test.js";
+
+export default {
+  name: "created-test",
+  mixins: [Test],
+  data() {
+    return {};
+  },
+  async created() {
+    console.log("333333333333");
+    await new Promise((resolve) => {
+      console.log("44444444444");
+      resolve();
+    })
+      .then(() => {
+        console.log("66666666666");
+      })
+      .then(() => {
+        console.log("8888888888888");
+      });
+    console.log("1010101010101010");
+  },
+  async mounted() {
+    console.log("【mounted】333333333333");
+    await new Promise((resolve) => {
+      console.log("【mounted】44444444444");
+      resolve();
+    })
+      .then(() => {
+        console.log("【mounted】66666666666");
+      })
+      .then(() => {
+        console.log("【mounted】8888888888888");
+      });
+    console.log("【mounted】1010101010101010");
+  },
+  methods: {},
+};
+</script>
